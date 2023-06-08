63269cdc9ab8cf7f30ffd66938b2e50a5d22d19a
munimulinfo <munimul.info@gmail.com>
2023-06-09 04:43:20 +0600

    user select button clicked and this class infomation post on data base this function implement

--- src/Layout/Pages/StudentViewClass/Classes.jsx
+++ src/Layout/Pages/StudentViewClass/Classes.jsx
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import useAxiosSecure from '../../../Hooks/useAxiosSequre';
 import { useQuery } from '@tanstack/react-query';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../Providers/AuthProviders';
+import useSilectClass from '../../../Hooks/useSelectClass';
+import Swal from 'sweetalert2';
 
 const Classes = () => {
+    const {user} = useContext(AuthContext);
+    const [, refetch] = useSilectClass();
+    const navigate = useNavigate();
+    const location = useLocation();
 
     const [axiosSecure] = useAxiosSecure();
-    const { data: allclass = [], refetch } = useQuery(['allclass'], async () => {
+    const { data: allclass = [] } = useQuery(['allclass'], async () => {
         const res = await axiosSecure.get('/allclasses')
         return res.data;
     })
     const aproved = allclass?.filter(classes => classes?.status === 'aproved');
-    console.log(aproved);
-
+   
+    const handleSelectClass = selectedClass => {
+        const {classname,image,instructoremail,instructorname,price,seats} = selectedClass || {};
+        if(user && user.email){
+            const selectClass = {classname, image, instructoremail, email: user?.email, instructorname, price, seats};
+            fetch('http://localhost:5000/selectclass',{
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(selectClass)
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.insertedId){
+                    refetch(); // refetch cart to update the number of items in the cart
+                    Swal.fire({                      
+                        icon: 'success',
+                        title: 'Class selected',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'Please login and select this class',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login', {state: {from: location}})
+                }
+              })
+        }
+    }; 
     return (
         <div>
             <h1 className='text-3xl text-center mt-24 font-semibold font-sans'>All instructor classes are available here.</h1>
@@ -27,7 +73,7 @@ const Classes = () => {
                             <p className='text-lg font-semibold font-sans '>Available seats: {aprovedClass?.seats}</p>
                             <p className='text-lg font-semibold font-sans '>Price: ${aprovedClass?.price}</p>
                             <div className="card-actions justify-end">
-                               <button className="btn bg-purple-500 bordr-0 text-white">Select</button>
+                               <button onClick={() => handleSelectClass(aprovedClass)} className="btn bg-purple-500 bordr-0 text-white">Select</button>
                             </div>
                         </div>
                     </div>)
